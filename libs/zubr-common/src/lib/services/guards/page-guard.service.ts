c30b5cdd859d55fba749b5fc1815fa3521174716
Alherd <algerd.blr@gmail.com>
2020-11-04 18:33:07 +0200

    Обновляю мета теги

--- libs/zubr-common/src/lib/services/guards/page-guard.service.ts
+++ libs/zubr-common/src/lib/services/guards/page-guard.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
@@ -6,44 +6,32 @@ import {
   RouterStateSnapshot,
   UrlTree
 } from '@angular/router';
-import { ReducerManagerDispatcher, Store } from '@ngrx/store';
+import {ReducerManagerDispatcher, Store} from '@ngrx/store';
 import {
   pageActions, AppState,
   PageTab
 } from '@zubr-client/zubr-store';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {Meta, Title} from "@angular/platform-browser";
+import {TranslateService} from "@ngx-translate/core";
 
-/**
- * Page accessibility service
- * @description
- * @export
- * @class PageGuardService
- * @implements {CanActivate}
- */
 @Injectable({
   providedIn: 'root',
 })
 export class PageGuardService implements CanActivate {
   public constructor(
     private _dispatcher: ReducerManagerDispatcher,
-    private _store$: Store<AppState>
-  ) {}
+    private _store$: Store<AppState>,
+    private title: Title,
+    private meta: Meta,
+    private translate: TranslateService
+  ) {
+  }
 
-  /**
-   * Checks for eligibility to enter route
-   * @description
-   * @param {ActivatedRouteSnapshot} route
-   * @param {RouterStateSnapshot} state
-   * @returns {(Observable<boolean | UrlTree>
-   *     | Promise<boolean | UrlTree>
-   *     | boolean
-   *     | UrlTree)}
-   * @memberof PageGuardService
-   */
   public canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
+    state: RouterStateSnapshot,
   ):
     | Observable<boolean | UrlTree>
     | Promise<boolean | UrlTree>
@@ -54,17 +42,23 @@ export class PageGuardService implements CanActivate {
       url: state.url,
       metadata: routeMetadata,
     };
+
+    this.translate.get(routeMetadata.title).subscribe(translation => {
+      this.title.setTitle(`${translation} | ZUBR`);
+
+      this.meta.updateTag({content: `ZUBR - ${translation}`}, "property='og:title'")
+      this.meta.updateTag({content: `ZUBR - ${translation}`}, "name='title'")
+    });
+
+    this.meta.updateTag({content: window.location.origin + state.url}, "property='og:url'")
+
     if (routeMetadata.navigateToPageTab) {
-      this._store$.dispatch(pageActions.ChangePageTabAction({
-        payload: pageTab,
-      }));
+      this._store$.dispatch(pageActions.ChangePageTabAction({payload: pageTab,}));
 
       return this._dispatcher.pipe(
-
         map(action => {
           return !(action.type === pageActions.PageTabsLimitExceededAction.type);
         })
-
       );
     }
 
